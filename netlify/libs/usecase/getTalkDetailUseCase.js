4cc87b43420c2a130dcc5b56d80adad911ad2c4f
Dimas Saputra <dimas.maulanads@gmail.com>
2022-09-25 17:50:25 +0700

    add object to user parent

--- netlify/libs/usecase/getTalkDetailUseCase.js
+++ netlify/libs/usecase/getTalkDetailUseCase.js
@@ -3,6 +3,7 @@ const NotFoundError = require('../exceptions/NotFoundError');
 async function getTalkDetailUseCase({ id }, { repository }) {
   const talk = await repository.getTalkById(id);
   let parent = null;
+  let userParent = null;
 
   if (!talk) {
     throw new NotFoundError('talk not found');
@@ -15,13 +16,17 @@ async function getTalkDetailUseCase({ id }, { repository }) {
   const user = await repository.getUserById(talk.user);
   const likes = await repository.getLikesByTalkId(talk.id);
 
+  if (parent) {
+    userParent = await repository.getUserById(parent.user);
+  }
+
   delete talk.replyTo;
 
   return {
     ...talk,
     user,
     likes: likes.map((like) => like.user_id),
-    parent,
+    parent: parent ? { ...parent, user: userParent } : null,
   };
 }
 
